4c2d14553f26a01886e00b4600570cc88fdb9bf8
kabanoff-i <iliakabanov1807@gmail.com>
2023-05-16 00:01:30 +0300

    first

---
+++ TicTacToe/Start.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace TicTacToe
+{
+    partial class Start
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBoxForX = new System.Windows.Forms.CheckBox();
+            this.checkBoxFor0 = new System.Windows.Forms.CheckBox();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBoxForX
+            // 
+            this.checkBoxForX.AutoSize = true;
+            this.checkBoxForX.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBoxForX.Location = new System.Drawing.Point(36, 119);
+            this.checkBoxForX.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBoxForX.Name = "checkBoxForX";
+            this.checkBoxForX.Size = new System.Drawing.Size(33, 17);
+            this.checkBoxForX.TabIndex = 0;
+            this.checkBoxForX.Text = "X";
+            this.checkBoxForX.UseVisualStyleBackColor = true;
+            this.checkBoxForX.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBoxFor0
+            // 
+            this.checkBoxFor0.AutoSize = true;
+            this.checkBoxFor0.Location = new System.Drawing.Point(101, 119);
+            this.checkBoxFor0.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBoxFor0.Name = "checkBoxFor0";
+            this.checkBoxFor0.Size = new System.Drawing.Size(32, 17);
+            this.checkBoxFor0.TabIndex = 1;
+            this.checkBoxFor0.Text = "0";
+            this.checkBoxFor0.UseVisualStyleBackColor = true;
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(22, 68);
+            this.textBoxX.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxX.Multiline = true;
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(53, 32);
+            this.textBoxX.TabIndex = 2;
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(101, 68);
+            this.textBoxY.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxY.Multiline = true;
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(55, 32);
+            this.textBoxY.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 76);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "x";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 160);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 34);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 15);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 30);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Tic-Tac-Toe";
+            // 
+            // Start
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(190, 214);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.checkBoxFor0);
+            this.Controls.Add(this.checkBoxForX);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Start";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxForX;
+        private System.Windows.Forms.CheckBox checkBoxFor0;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ TicTacToe/Start.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Start : Form
+    {
+
+        public static int SetValueForX;
+        public static int SetValueForY;
+        public Start()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBoxX.Text != null && textBoxY.Text != null
+                && !string.IsNullOrWhiteSpace(textBoxX.Text)
+                && !string.IsNullOrWhiteSpace(textBoxY.Text))
+            {  
+            SetValueForX = Convert.ToInt32(textBoxX.Text);
+            SetValueForY = Convert.ToInt32(textBoxY.Text);
+                if (SetValueForX == SetValueForY)
+                {
+                    Start start = new Start();
+                    if(checkBoxForX.Checked)
+                    {
+                
+                        Form1 frm1 = new Form1(SetValueForX, SetValueForY, checkBoxForX.Text);
+                        this.Hide();
+
+                        frm1.Closed += (s, args) => this.Close();
+
+                        frm1.Show();
+                    }
+                    if (checkBoxFor0.Checked)
+                    {
+                 
+                        Form1 frm1 = new Form1(SetValueForX, SetValueForY, checkBoxFor0.Text);
+                        this.Hide();
+
+                        frm1.Closed += (s, args) => this.Close();
+
+                        frm1.Show();
+                    }
+
+         
+                }
+                else
+                {
+                    MessageBox.Show("We can't create Tic-Tac-Toe game for " +
+                    SetValueForX + "x" + SetValueForY + " table X-O");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Fill Values");
+            }
+        }
+    }
+}
