97ecd4bbacac6043bc62c1ab01f1fadca3c1c607
kabanoff-i <iliakabanov1807@gmail.com>
2023-05-26 23:04:24 +0300

    final

--- TicTacToe/FilledCellException.cs
+++ TicTacToe/FilledCellException.cs
@@ -12,11 +12,4 @@ namespace TicTacToe
         public FilledCellException(string message) : base(message) { }
         public FilledCellException(string message, Exception e): base(message, e) { }
     }
-    public class OutOf5RangeCellsException: Exception
-    {
-        public OutOf5RangeCellsException() { }
-        public OutOf5RangeCellsException(string message) : base(message) { }
-        public OutOf5RangeCellsException(string message, Exception e) : base(message, e) { }
-
-    }
 }
---
+++ TicTacToe/OutOf5RangeCellsException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class OutOf5RangeCellsException : Exception
+    {
+        public OutOf5RangeCellsException() { }
+        public OutOf5RangeCellsException(string message) : base(message) { }
+        public OutOf5RangeCellsException(string message, Exception e) : base(message, e) { }
+
+    }
+}
